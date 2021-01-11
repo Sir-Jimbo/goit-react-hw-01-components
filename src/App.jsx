3f023d70f846406310286bb883df8cc5861812ec
Alexey Golodenko <alexey.golodenko@gmail.com>
2021-01-11 21:41:31 +0200

    Все задачи со стилями

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import ProfileList from './components/Profile/ProfileList';
 import Statistics from './components/Statistics';
+import FriendList from './components/FriendList';
+import TransactionHistory from './components/TransactionHistory';
 import user from './components/Profile/user.json';
 import statisticalData from './components/Statistics/statistical-data.json';
+import friend from './components/FriendList/friends.json';
+import transactions from 'components/TransactionHistory/transactions.json';
 
 export default function App() {
   return (
@@ -9,6 +13,8 @@ export default function App() {
       <ProfileList items={user} />
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
+      <FriendList friends={friend} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
